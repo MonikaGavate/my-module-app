439591c23e6214ae702108e57d10c6ee38d25780
git config --global user.email gavtemonika@gmail.com <gavtemonika@gmail.com>
2022-05-08 12:19:03 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AdminModule } from './admin/admin.module';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [
+ AppComponent
+],
+ imports: [
+ BrowserModule,
+ AppRoutingModule,
+ AdminModule
+],
+ providers: [],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
